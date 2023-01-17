9959977b74fd1c8d4656451da8964b4867efeba2
gtgambit <107627219+gtgambit@users.noreply.github.com>
2023-01-17 15:54:40 +0000

    Deploying to gh-pages from @ gtgambit/goit-react-hw-05-movies@a89c73af26da52d28abeb68d939b12e93ac0ed3d 🚀

--- static/js/335.b0b2cf99.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[335],{335:function(e,t,n){n.r(t);var r=n(791),u=n(689),c=n(184);t.default=function(){var e=(0,u.s0)();return(0,r.useEffect)((function(){var t=setTimeout((function(){e("/")}),5e3);return function(){return clearTimeout(t)}}),[e]),(0,c.jsx)("div",{children:(0,c.jsx)("p",{children:"Page not Found!"})})}}}]);
-//# sourceMappingURL=335.b0b2cf99.chunk.js.map
